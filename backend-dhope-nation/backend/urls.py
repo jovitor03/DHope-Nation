8e13e88a496c144fb06cf38c1c3a0ebae5304f50
João Vítor <123700107+jovitor03@users.noreply.github.com>
2024-11-19 18:07:21 +0000

    Fix urls

--- backend-dhope-nation/backend/urls.py
+++ backend-dhope-nation/backend/urls.py
@@ -20,24 +20,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from DHopeNation import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login',views.login),
-    path('register',views.register),
-    path('profile/donator',views.profile_donator),
-    path('profile/donator/delete',views.delete_account),#BRO esta aqui o delete do donator
-    path('profile/donator/edit',views.edit_profile),
-    path('profile/campign-creator',views.profile_campaign_creator),
-    path('profile/campaign-creator/edit',views.edit_profile),
-    path('profile/campaign-creator/my_campaigns',views.get_campaigns_by_creator),
-    path('profile/campaign-creator/delete',views.delete_account), #BRO esta aqui o delete do campaign creator
-    path('campaign/create',views.create_campaign),
-    path('campaign/get',views.get_campaigns),
-    path('donator_get',views.donar_count),
-    path('campaign/donate',views.donate),#BRO esta qui o donate
-    path('get_all_donators',views.get_all_donators),
-    path('get_all_campaign_creators',views.get_all_campaign_creators),
-    path('get_all_campaigns',views.get_all_campaigns),
-   
+path('admin/', admin.site.urls),
+    re_path('login',views.login),
+    re_path('register',views.register),
+    re_path('profile/donator',views.profile_donator),
+    re_path('donator/delete',views.delete_account),#BRO esta aqui o delete do donator
+    re_path('profile/donator/edit',views.edit_profile),
+    re_path('profile/campign-creator',views.profile_campaign_creator),
+    re_path('profile/campaign-creator/edit',views.edit_profile),
+    re_path('profile/campaign-creator/my_campaigns',views.get_campaigns_by_creator),
+    re_path('campaign-creator/delete',views.delete_account), #BRO esta aqui o delete do campaign creator
+    re_path('campaign/create',views.create_campaign),
+    re_path('campaign/get',views.get_campaigns),
+    re_path('donator_get',views.donar_count),
+    re_path('campaign/donate',views.donate),#BRO esta qui o donate
+    re_path('get_all_donators',views.get_all_donators),
+    re_path('get_all_campaign_creators',views.get_all_campaign_creators),
+    re_path('get_all_campaigns',views.get_all_campaigns),
+
 ]
 
 if settings.DEBUG:
