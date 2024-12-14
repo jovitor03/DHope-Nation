68c7ed70bfb77a2186cd0071bbbb82e1d8434c14
João Vítor <123700107+jovitor03@users.noreply.github.com>
2024-12-14 18:45:15 +0000

    Delete honor system file

--- dhope-nation/src/utils/HonorSystem.js
+++
@@ -1,5 +0,0 @@
-//if donations in the last 30 days are less than 50€ but greater than 25€, the user will receive "High Honor"
-
-//if donations in the last 30 days are equal or greater than 50€, the user will receive "Super High Honor"
-
-//if donations in the last 30 days are less than 25€, the user will receive "Neutral Honor"
